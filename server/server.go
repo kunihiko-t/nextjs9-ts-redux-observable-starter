3843a580e1baefa1adcc8d5baa027a800ba06cd4
Kunihiko Tanaka <inverse.of.void.matrix@gmail.com>
2020-05-10 01:54:30 +0900

    fix websocket

--- server/server.go
+++ server/server.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/kunihiko-t/nextjs9-ts-redux-observable-starter/server/gqlgen-todos/graph"
 	"github.com/kunihiko-t/nextjs9-ts-redux-observable-starter/server/gqlgen-todos/graph/generated"
 	"github.com/rs/cors"
-	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
+
+func getServer() *handler.Server {
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		// Add this lines
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	srv.AddTransport(
+		&transport.Websocket{Upgrader: upgrader,
+			KeepAlivePingInterval: 10 * time.Second,
+		})
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+
+	srv.SetQueryCache(lru.New(1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
+	return srv
+}
 
 func main() {
 	router := chi.NewRouter()
@@ -25,17 +58,7 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		// Add this lines
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-	srv.AddTransport(&transport.Websocket{Upgrader: upgrader})
-
+	srv := getServer()
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
